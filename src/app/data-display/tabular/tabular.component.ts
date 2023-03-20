b2633783d84eded4eff677a11b14303a8a95f3ca
shamim <shamimahmad586@gmail.com>
2023-03-20 17:09:32 +0530

    tabular data

--- src/app/data-display/tabular/tabular.component.ts
+++ src/app/data-display/tabular/tabular.component.ts
@@ -15,54 +15,10 @@ export class TabularComponent {
   private gridColumnApi!: ColumnApi;
   locationType:any
 
+  ngOnInit() {
+    alert("called");
+  }
 
-  // public columnDefs: ColDef[] = [
-  //   { field: 'athlete', filter: 'agTextColumnFilter', minWidth: 200 },
-  //   { field: 'age',enableRowGroup: true },
-  //   { field: 'country', minWidth: 180 },
-  //   { field: 'year' },
-  //   { field: 'date', minWidth: 150 },
-  //   { field: 'gold' },
-  //   { field: 'silver' },
-  //   { field: 'bronze' },
-  //   { field: 'total' },
-  // ];
-  // public defaultColDef: ColDef = {
-  //   flex: 1,
-  //   minWidth: 100,
-  //   // allow every column to be aggregated
-  //   enableValue: true,
-  //   // allow every column to be grouped
-  //   enableRowGroup: true,
-  //   // allow every column to be pivoted
-  //   enablePivot: true,
-  //   sortable: true,
-  //   filter: true,
-  // };
-  // public autoGroupColumnDef: ColDef = {
-  //   minWidth: 200,
-  // };
-  // public rowData!: IOlympicData[];
-
-  // sideBar = {
-  //   toolPanels: [
-  //     {
-  //       id: 'columns',
-  //       labelDefault: 'Columns',
-  //       labelKey: 'columns',
-  //       iconKey: 'columns',
-  //       toolPanel: 'agColumnsToolPanel',
-  //     },
-  //     {
-  //       id: 'filters',
-  //       labelDefault: 'Filters',
-  //       labelKey: 'filters',
-  //       iconKey: 'filter',
-  //       toolPanel: 'agFiltersToolPanel',
-  //     }
-  //   ],
-  //   defaultToolPanel: 'columns'
-  // }
 
   public columnDefs: ColDef[] = [
     { field: 'country',filter:'agSetColumnFilter'},
