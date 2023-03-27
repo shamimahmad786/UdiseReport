331e2de821deca8379883c761fe6a4a2eb29d1a7
Dharmendra <dharmendraprajapatixds@gmail.com>
2023-03-27 15:10:51 +0530

    animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit
+ {
   title = 'UdiseReport';
+
+  ngOnInit(){
+    AOS.init();
+    AOS.refresh();
+
+  }
 }
