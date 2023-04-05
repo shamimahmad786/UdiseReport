6c376b2a2ba8fcc84f6564569bd2367dc4e23b9d
Ali Ahamad <developerahamadali@gmail.com>
2023-04-05 17:01:02 +0530

    change

--- src/app/service/tabular-data-service.component.ts
+++ src/app/service/tabular-data-service.component.ts
@@ -11,9 +11,15 @@ export class TabularDataService {
   
   getTabularData(data:any)
   {
-    console.log("sfjkdjfnlkjd " + JSON.stringify(data));
-    //let data1={"mapId":"1001","reportFor":"1","initYear":2021,"cateoryType":9,"managementType":9,"dependency":{"stateId":"110","paramValue":"as"}};
+   // console.log("sfjkdjfnlkjd " + JSON.stringify(data));
+   // let data1={"mapId":"1001","reportFor":"1","initYear":2021,"cateoryType":9,"managementType":9,"dependency":{"stateId":"110","paramValue":"as"}};
     return this.http.post<any>(environment.BASE_URL + "/api/getTabularData", data);
+
+  }
+
+  getReportData(data:any){
+    let data1={"mapId":"1002","reportFor":"1","initYear":2021,"cateoryType":9,"managementType":9,"dependency":{"stateId":"110","paramValue":"as"}};
+    return this.http.post<any>(environment.BASE_URL + "/api/getSunBurstData", data1);
   }
 
 
